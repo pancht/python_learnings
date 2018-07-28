2c862621362d0f80c151f8195a1e7a7da00c1fdc
pancht <erpanchdev@gmail.com>
2018-07-28 12:54:17 -0700

    str.center(width[, fillchar])¶

---
+++ 0003 Strings/string_methods.py
@@ -0,0 +1,10 @@
+# str.capitalize()
+print("panchdev chauhan".capitalize()) # OUTPUT => Panchdev chauhan
+
+# str..casefold()
+print('ß'.casefold()) # 'ß' is a german letter
+print('ß'.lower())
+
+# str.center(width[, fillchar])¶
+print("Panchdev Chauhan".center(20,"=")) # OUTPUT => ==Panchdev Chauhan==
+
