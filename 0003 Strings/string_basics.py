219b0a48ec1c563a521ea8597188d0f3d51dd833
pancht <erpanchdev@gmail.com>
2018-07-28 06:23:28 -0700

    Program to Print Pattern

--- 0003 Strings/string_basics.py
+++ 0003 Strings/string_basics.py
@@ -23,4 +23,42 @@ you can use raw strings by adding an r before the first quote:
 print(r"C:\\home\\learning")
 
 
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+
+"""
+  /\
+ /  \
+/    \
+Previous one produces following OUTPUT:
+=======================================
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+"""
+
+# Strings can be concatenated (glued together) with the + operator, and repeated with *:
+print("Er." + "Panchdev Chauhan")
+
+"""
+Program To Print Pattern
+=========================
+         *
+        ***
+       *****
+      *******
+     *********
+    ***********
+   *************
+  ***************
+ *****************
+"""
+for counter in range(1, 10):
+    import sys
+    sys.stdout.write(" " * (10 - counter))
+    print("*" * (((counter-1) * 2) + 1))
+
 
