6d2661d97bdc32c4220e188833c07a895abaa04a
pancht <erpanchdev@gmail.com>
2018-07-28 18:34:27 -0700

    str.find(sub[, start[, end]])

--- 0003 Strings/string_methods.py
+++ 0003 Strings/string_methods.py
@@ -18,4 +18,16 @@ print("To err is human nature.".endswith(".",7))
 # str.expandtabs(tabsize=8)
 print("Sr. No.\tName\tSurname\tAddress\tPhone\tMobile".expandtabs(10))
 
+# str.find(sub[, start[, end]])
+print("""
+This is a multi-line Text.
+This is second line.
+This is third line.
+""".find("is",20,100))
+
+print("""
+This is a multi-line Text.
+This is second line.
+This is third line.
+""".find("Apple",20,100))  # Will return -1 as Apple is not present in Text
 
